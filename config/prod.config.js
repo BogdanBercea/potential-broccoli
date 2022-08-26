f527910c2a8a63811f9195248f772d6498e6c242
Bogdan Bercea <bogdanbercea6@gmail.com>
2022-08-26 19:46:24 +0300

    Add prod and dev keys configuration

---
+++ config/prod.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID, //' 1743674244-gjcho0c8t3gprr7p3gr0ldmipvphtnhi.apps.googleusercontent.com',
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,// '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',
+  mongoURI: process.env.MONGO_URI,// 'mongodb+srv://emaily-production-db:pBwJ7CpLgEN4vqV2@cluster0.mstyx71.mongodb.net/emailydb-production?retryWrites=true&w=majority',
+  cookieKey: process.env.COOKIE_KEY // 'asdfdsfa12312sfeasvesfasfs3213123egsdxvessv4bakjbdaskjbfsdkljalkgacmnsklnvadlksdnadsajtfgas',
+}
